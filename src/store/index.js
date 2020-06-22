604314af5c90052b1f614b85d640c3b5549d7a66
jaoaoun <ntd_pst@hotmail.com>
2020-06-23 01:50:01 +0700

    Part2: make for editbook

--- src/store/index.js
+++ src/store/index.js
@@ -10,12 +10,20 @@ export default new Vuex.Store({
   mutations: {
     addBook(state,payload){
       state.books.push(payload)
+    },
+    //1)สร้างเพิ่ม เพื่อให้สามารถ editbook ได้
+    editBook(state,payload){
+      state.books[payload.index] = payload.data
+      //2)booksของเราเป็นarray เลยให้letไปที่indexของค่าที่editมา และให้มีค่า=data (คือใหม่ไปทับเก่าเลย)
     }
   },
   actions: {
     addBook({commit},payload){
       // console.log("test addbook",payload)
       commit("addBook",payload)
+    },
+    editBook({commit},payload){
+      commit("editBook",payload) //3)commitไปที่editBookแล้วส่งpayloadไป
     }
   },
   modules: {
