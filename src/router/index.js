2ac453f7861997c93ac7b0a016159532574e4011
jaoaoun <ntd_pst@hotmail.com>
2020-06-22 20:01:51 +0700

    Part1: add path AddBook in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import AddBook from '../views/AddBook.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
+    path: '/add-book',
+    name: 'add-book',
+    component: AddBook
+  },
+  {
     path: '/about',
-    name: 'About',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
