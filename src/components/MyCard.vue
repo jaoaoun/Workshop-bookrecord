15a0afe03df60aa1969b9b2da923269b4fb655ef
jaoaoun <ntd_pst@hotmail.com>
2020-06-22 23:16:15 +0700

    Part2:get data from store to show in card

--- src/components/MyCard.vue
+++ src/components/MyCard.vue
@@ -2,18 +2,18 @@
   <div class="card mb-3" style="min-width:400px;max-width:538px;">
     <div class="row no-gutters">
       <div class="col-md-4">
-        <img src="https://data.whicdn.com/images/331489284/original.jpg" class="card-img" alt />
+        <img :src="bookDetail.cover" class="card-img" alt />
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">test title</h5>
-          <p class="card-text">test tesssst</p>
+          <h5 class="card-title">{{bookDetail.name}}</h5>
+          <p class="card-text">{{bookDetail.description}}</p>
           <p class="card-text">
-            <small class="text-muted">By En </small>
+            <small class="text-muted">By {{bookDetail.author}} </small>
           </p>
           <div class="small-box">
-            <small class="text-small">DATE: 04/06/2020</small>
-            <small class="text-small rating"> RATING: <b>5</b></small>
+            <small class="text-small">DATE: {{bookDetail.date}}</small>
+            <small class="text-small rating"> RATING: <b>{{bookDetail.rating}}</b></small>
           </div>
           <div class="small-box">
             <router-link class="btn btn-primary">EDIT</router-link>
