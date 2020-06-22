269c377867aed89c5f152cc534f808ef424d1c5f
jaoaoun <ntd_pst@hotmail.com>
2020-06-22 20:01:09 +0700

    Part1: create AddBook and imp Form

---
+++ src/views/AddBook.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>Please Add Book</h1>
+    <div class="box">
+      <my-form />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MyForm from "../components/MyForm";
+export default {
+  name: "AddBook",
+  components: {
+    MyForm,
+  },
+};
+</script>
+
+<style lang="css">
+.box{
+    width: 400px;
+    margin: 50px auto;
+}
+
+</style>
